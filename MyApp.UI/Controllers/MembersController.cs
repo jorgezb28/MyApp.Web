5d786c57f9c01b78875aece89f65da4479f7dbbc
Cristhian Motino <cristhianmoti@hotmail.com>
2024-02-26 14:14:07 -0600

    Add project files.

---
+++ MyApp.UI/Controllers/MembersController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.UI.Controllers
+{
+    public class MembersController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddMember()
+        {
+            return View();
+        }
+
+        public ActionResult EditMember()
+        {
+            return View();
+        }
+
+        public ActionResult ViewDetails()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MyApp.UI/Models/Member.cs
@@ -0,0 +1,12 @@
+﻿namespace MyApp.UI.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public DateTime JoinDate { get; set; }
+        public MembershipPlan MembershipPlan { get; set; }
+    }
+}
